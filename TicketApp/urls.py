19bec531663b5d4be009cbb6ab48a0ef9d3fae90
Andres Torres <magoyde@gmail.com>
2020-12-29 01:09:17 -0500

    añadido ruta Agency

--- TicketApp/urls.py
+++ TicketApp/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import url
 #llamamos  los views de la app
 from TicketApp import views
 
-urlpatterns=[
     #agragamos las rutas de los views
+urlpatterns=[
     url(r'^User/$',views.userApi),
-    url(r'^Agency/$',views.userApi),
-    url(r'^Customer/$',views.userApi),
-    url(r'^Client/$',views.userApi),
-    url(r'^Tickets/$',views.userApi),
-
+    url(r'^Agency/$',views.agencyApi)
+    # url(r'^Customer/$',views.customerApi),
+    # url(r'^Client/$',views.clientApi),
+    # url(r'^Tickets/$',views.ticketApi),
 ]
